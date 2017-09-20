3aaf1a95d8c8b3d8d0871f8b5bbf0fa06e52855f
tomjscox <tom.cox@uantwerp.be>
2017-09-20 09:31:37 +0200

    Solve CRAN check issues

--- R/GPPFourier.R
+++ R/GPPFourier.R
@@ -555,36 +555,8 @@ demod <- function(	x, 				# Time series to be demodulated
 # #' @details no details
 #' @examples 
 
-#' 
-#' # Calculate GPP(t) by complex demodulation of Hoernum Tief O2 time series
-#'
-#' dt <- as.numeric(diff(Hoernum$time)[1], units="days")
-#' Nf <- 1/dt
-#' phi <- 54.783
-#' lambda <- 8.45
-#' # Diurnal harmonic
-#' T1 <- 1
-#' f1 <- 1/T1
-#' # Tidal harmonics
-#' TO1 <- 25.81933871/24	# O1 period
-#' fO1 <- 1/TO1
-#' TQ1 <- 26.868350/24   # Large lunar elliptic diurnal
-#' fQ1 <- 1/TQ1
-#' 
-#' GPPt <- GPPFourier_t(gapfill(Hoernum), Nf=Nf, nf=2, NLowPass=Nf, phi=phi, lambda=lambda)
-#' GPPt15 <- GPPFourier_t(gapfill(Hoernum), Nf=Nf, nf=2, NLowPass=15/dt, phi=phi, lambda=lambda)
-#' 
-#' par(mfrow=c(2,1))
-#' plot(GPPt, type="l", ylab="GPP(t)")
-#' title(main="Hoernum Tief")
-#' lines(GPPt15, lwd=3)
-#' legend("topright", legend=c("1 Day filter", "15 Day filter"),lwd=c(1,3), bty="n") 
-#' plotF((GPPt$GPPt-GPPt15$GPPt)[!is.na(GPPt15$GPPt)], dt=dt, xlim=c(0,0.5), type="b", pch=20)
-#' abline(v=c(f1-fO1, f1-fQ1))
-#' title(main="Spectrum of difference")
-#' 
-#' 
 #' # Calculate GPP(t) by complex demodulation of simulated water column time series
+#' # More examples in vignette("GPPFourier").
 #' 
 #' dt <- as.numeric(difftime(watercolumn$time[2], watercolumn$time[1] , units="days"))
 #' Nfilt <- 1/dt
@@ -598,7 +570,7 @@ demod <- function(	x, 				# Time series to be demodulated
 #'                         fDL=NULL, 
 #'                         circular=FALSE, 
 #'                         sides=2, 
-#'                         nf=2)
+#'                         nf=1)
 #' 
 #' 
 #' par(mfrow=c(2,1), cex=1.2)
@@ -613,35 +585,6 @@ demod <- function(	x, 				# Time series to be demodulated
 #'          bty="n")
 #' 
 #' 
-#' # Calculate GPP(t) by complex demodulation ofsimulated estuary time series
-#' 
-#' dt <- as.numeric(difftime(estuary$time[2], estuary$time[1] , units="days"))
-#' Nfilt <- 1/dt
-#' GPPt <- GPPFourier_t(estuary[,c("time", "O2")], 
-#'                       dt=dt, 
-#'                       Detrend=FALSE, 
-#'                       filter=TRUE, 
-#'                       Nfilt=Nfilt, 
-#'                       NLowPass=Nfilt, 
-#'                       fDL=NULL, 
-#'                       circular=FALSE, 
-#'                       sides=2, 
-#'                       nf=2)
-
-#' par(mfrow=c(2,1), cex=1.2)
-#' plot(estuary[,c("time", "O2")], type="l", xlab="", ylab=expression(paste(O[2], " [", mu, "M]")))
-#' title(main="Estuary")
-#' plot(GPPt, type="l", lwd=3, xlab="", ylab="GPP", ylim=c(0,30))
-#' lines(estuary$time, filter(filter(estuary$GPP, rep(1/Nfilt, Nfilt)),rep(1/Nfilt, Nfilt)), 
-#'              type="l", 
-#'              lwd=3, 
-#'              ylim=c(0,30), 
-#'              xlab="", 
-#'              ylab="GPP", 
-#'              col="red")
-#' legend("topleft", lty=1, col=c("black", "red"), 
-#'          legend=c(expression(paste("Demodulated ", O[2], " series")), "GPP"), 
-#'          bty="n") 
 
 
 #' @references Cox et al (2017). Tune in on 11.57 muHz and listen to primary production. Biogeosciences Discussions. doi:10.5194/bg-2017-81
